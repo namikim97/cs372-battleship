bd54c073f092c17f873d1696230a4d57890094d3
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-02-09 14:07:29 -0900

    Add simple SFML

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+// main.cpp
+//
+// Created: 2 Feb 2019
+// Updated: 2 Feb 2019
+//
+// Simple program to illustrate SFML integration.
+// Copied from the SFML tutorial for Drawing 2-D Stuff.
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.display();
+	}
+
+	return 0;
+}
