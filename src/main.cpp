39bf9624d3c6fe1754ded5bbc5a54c343cd9f9f3
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-02-19 19:18:36 -0900

    Add basic text-based game functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 // main.cpp
 //
-// Created: 2 Feb 2019
-// Updated: 2 Feb 2019
+// Created: 9 Feb 2019
+// Updated: 19 Feb 2019
 //
-// Simple program to illustrate SFML integration.
-// Copied from the SFML tutorial for Drawing 2-D Stuff.
+// Simple program to illustrate text-based game.
+// Basic SFML app copied from the SFML tutorial (Drawing 2-D Stuff).
 
 #include <SFML/Graphics.hpp>
 #include "board.hpp"
-
+#include "textBasedGame.hpp"
 
 int main()
 {
+	// "ACTIVE" Illustrate basic text-based game
+	textBasedGame();
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship");
-
-	while (window.isOpen())
+	if (0) // "INACTIVE" A basic SFML app.
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship");
+
+		while (window.isOpen())
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear(sf::Color::Black);
+
+			window.display();
 		}
-
-		window.clear(sf::Color::Black);
-
-		window.display();
 	}
 
 	return 0;
