53657ebd59726b84a94399f08a1162266e968c4a
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-02-24 21:40:33 -0900

    Add basic graphic game

---
+++ include/local_gfxGame.hpp
@@ -0,0 +1,27 @@
+// local_gfxGame.hpp
+//
+// Created: 24 Feb 2019
+// Updated: 24 Feb 2019
+//
+// Header for local PC graphics-based Battleship game.
+
+#ifndef FILE_LOCAL_GFXGAME_HPP_INCLUDED
+#define FILE_LOCAL_GFXGAME_HPP_INCLUDED
+
+#include <SFML/Graphics.hpp>
+// For sf::RenderWindow, sf::Vector2f, sf::Event, etc
+
+#include <iostream>
+// For std::cout, std::endl;
+
+#include "gfxBoard.hpp"
+#include "board.hpp"
+
+
+// ********************************************************************
+// PUBLIC FUNCTIONS
+// ********************************************************************
+
+void local_gfxGame();
+
+#endif // #ifndef FILE_LOCAL_GFXGAME_HPP_INCLUDED
